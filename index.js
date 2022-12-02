d27082e326012bcae5d4ba5718ba449b2cac63ab
Isabel-P <izzyschoolwork@gmail.com>
2022-12-02 16:49:57 -0500

    listening

---
+++ index.js
@@ -0,0 +1,8 @@
+const { request } = require('express');
+const express = require('express'); //allows me to use it
+const path    = require('path');
+const server  = express();
+const port    = 3000;//port the server will run on
+server.use(express.json()); //allows for parsing into jason
+
+server.listen (port, () => console.log ('server is now listening on port '+ port)); 
