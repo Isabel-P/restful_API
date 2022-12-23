c2f61a2fc992f954c40818aac13234c88c976c96
Isabel-P <izzyschoolwork@gmail.com>
2022-12-22 22:56:11 -0500

    Done

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ const path    = require('path');
 const server  = express();
 const port    = 3000;//port the server will run on
 server.use(express.json()); //allows for parsing into jason
+app.use(express.urlencoded({extended: false}));
+
+
+app.get('/food', db.getFood)
+app.get('/food/:id', db.getFoodById)
+app.post('/food', db.createFood)
+app.put('/food/:id', db.updateFood)
+app.delete('/food/:id', db.deleteFood)
+
 
 server.listen (port, () => console.log ('server is now listening on port '+ port)); 
